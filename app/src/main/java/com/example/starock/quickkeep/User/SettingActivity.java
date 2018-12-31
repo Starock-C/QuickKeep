c7a1eae0cb67cb6f9db0336e614336a7c8558316
yanl <284545631@qq.com>
2018-12-31 23:09:13 +0800

    setting ui

--- app/src/main/java/com/example/starock/quickkeep/User/SettingActivity.java
+++ app/src/main/java/com/example/starock/quickkeep/User/SettingActivity.java
@@ -10,6 +10,6 @@ public class SettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_main);
+        setContentView(R.layout.activity_user_setting);
     }
 }
