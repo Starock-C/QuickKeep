07bcfffbd817685805b24de1d3f5502ff93b4680
yanl <284545631@qq.com>
2019-01-09 15:06:19 +0800

    setting ui

---
+++ app/src/main/java/com/example/starock/quickkeep/User/AboutusActivity.java
@@ -0,0 +1,14 @@
+package com.example.starock.quickkeep.User;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.starock.quickkeep.R;
+
+public class AboutusActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_aboutus);
+    }
+}
